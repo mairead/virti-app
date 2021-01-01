458e9cfb8c5534b92b2c96d91d3f35dca8081be5
Mairead Buchan <mairead.buchan@headlondon.com>
2021-01-01 14:25:12 +0000

    build folder

---
+++ client/build/precache-manifest.974fbcfcefbe8af61dfd18d7c468e8e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c05cfe1456b1b3d6966851a4af5b155a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd81c6808e7e928d6245",
+    "url": "/static/css/main.f99158eb.chunk.css"
+  },
+  {
+    "revision": "f5a98beeda59e937ea8b",
+    "url": "/static/js/2.531a9c94.chunk.js"
+  },
+  {
+    "revision": "9b9de423b52b798b8fc649e7d9e276fb",
+    "url": "/static/js/2.531a9c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd81c6808e7e928d6245",
+    "url": "/static/js/main.12563624.chunk.js"
+  },
+  {
+    "revision": "f01171ac1b37d8e62b60",
+    "url": "/static/js/runtime-main.3618ac1b.js"
+  }
+]);
