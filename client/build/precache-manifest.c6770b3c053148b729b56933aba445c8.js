c4807b8d2e9757af8cd15c5d3a8785318256bd92
Mairead Buchan <mairead.buchan@headlondon.com>
2021-01-01 20:12:03 +0000

    build scripts

---
+++ client/build/precache-manifest.c6770b3c053148b729b56933aba445c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8d0cbb10908108579960d2262bf7b3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "204b3ecea569d4416c3d",
+    "url": "/static/css/main.44c08cc9.chunk.css"
+  },
+  {
+    "revision": "f5a98beeda59e937ea8b",
+    "url": "/static/js/2.531a9c94.chunk.js"
+  },
+  {
+    "revision": "9b9de423b52b798b8fc649e7d9e276fb",
+    "url": "/static/js/2.531a9c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "204b3ecea569d4416c3d",
+    "url": "/static/js/main.554247fa.chunk.js"
+  },
+  {
+    "revision": "f01171ac1b37d8e62b60",
+    "url": "/static/js/runtime-main.3618ac1b.js"
+  }
+]);
