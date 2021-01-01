7cc0b9e4183fb4456076b1d3fc6bdefd8a2c6326
Mairead Buchan <mairead.buchan@headlondon.com>
2021-01-01 20:19:47 +0000

    adding deploy variables

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 'use strict';
 
+require('dotenv').config();
 var os = require('os');
 var nodeStatic = require('node-static');
 var http = require('http');
 var socketIO = require('socket.io');
 
-var fileServer = new(nodeStatic.Server)();
+const port = process.env.PORT || 8080;
+
+var fileServer = new(nodeStatic.Server)('./client/build');
 var app = http.createServer(function(req, res) {
   fileServer.serve(req, res);
-}).listen(8080);
+}).listen(port);
 
 var io = socketIO(app, {
   cors: {
